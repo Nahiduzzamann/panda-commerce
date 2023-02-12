0e72b02c164273a9b13a2cd1cdb1d6da3638a6a9
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-12 12:30:45 +0000

    ok

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ const cards = document.getElementsByClassName('card');
 for(let card of cards){
     card.style.borderRadius= '30px'
 }
+
+function consol(){
+    document.getElementById('remove').style.display='none'
+    
+}
+
+document.getElementById('exampleInputEmail1').addEventListener('keyup',function(event){
+    const text = event.target.value;
+    console.log(text);
+    if(text==='email'){
+        document.getElementById('form-btn').removeAttribute('disabled')
+    }
+    else{
+        document.getElementById('form-btn').setAttribute('disabled',true)
+    }
+})
+
+
+
+
+
