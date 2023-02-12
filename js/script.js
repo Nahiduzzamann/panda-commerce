884c2ed794bc363e0ba2175512384b2ba5d012cf
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-12 11:03:37 +0000

    use js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const h2s = document.getElementsByTagName('h2')
+for(let h2 of h2s){
+    h2.style.color='lightBlue'
+}
+
+document.getElementById('backpack').style.backgroundColor='tomato'
+
+const cards = document.getElementsByClassName('card');
+for(let card of cards){
+    card.style.borderRadius= '30px'
+}
